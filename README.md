24d69e193fdb511f3975842f881d6a8034c4201c
jekapiotr99 <56678518+jekapiotr99@users.noreply.github.com>
2022-01-24 18:35:23 +0100

    Zmiany w projekcie

--- README.md
+++ README.md
@@ -1,18 +1,123 @@
 # Vue.js Todo List
 
-> A Todo List made with Vue.js
+Strona wykorzystana jako przykład do analizy:<br>
+https://vuejsexamples.com/a-todo-list-made-with-vue-js/
 
-## Build Setup
+### Opis przykładu przed zmianami
+Wybrałem tworzenie listy todo, ponieważ w laboratorium 6 wybrałem ten sam typ zadania do nauki vue. Chciałem porównać czym
+moja aplikacja różni się od już gotowych rozwiązań dostępnych na stronie vuejsexamples.
 
-``` bash
-# install dependencies
-npm install
+Prezentacja strony:
 
-# serve with hot reload at localhost:8080
-npm run dev
+![obraz](https://user-images.githubusercontent.com/56678518/150824035-abbeb0c6-fd59-4965-bb14-96643184d47b.png)
 
-# build for production with minification
-npm run build
-```
+Strona prezentuje możliwości takie jak: dodawanie zadań, usuwanie zadań, odznaczanie już gotowych zadań. Różnica
+między moim Lab 6, a tym rozwiązaniem jest taka, że w moim rozwiązaniu zadania(przedmioty na liście zakupowej) były
+poprostu usuwane.
 
-For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
+Podczas przeglądania projektu wywnioskowałem, że projekt został utworzony za pomocą vue-cli.
+
+Możemy również zauważyć, że projekt został stworzony przez osobę która uczy się dopiero vue.
+
+![obraz](https://user-images.githubusercontent.com/56678518/150825383-d0abaa0c-4451-4bb8-8ed2-67d643a8740d.png)
+
+Wnioskuję z tego zdjęcia, gdzie komponent nazywa się test.vue, a zawartość jego to praktycznie pusty template.
+
+Całość aplikacji zawarta jest w pliku App.vue
+
+Template tego projektu zawarty jest w kontenerze, który podzielony został na 2 części. <br>
+- część 1 - nagłówkowa zawierająca:
+  - Logo,
+  - Nagłówek
+
+- część 2 - ToDo list zawiera:
+  - Formularz do wpisywania,
+  - Wypisane taski
+
+![obraz](https://user-images.githubusercontent.com/56678518/150825677-f530f4b0-77ef-4e8c-bfa5-2518b1bc1285.png)
+
+Cześć skryptowa wygląda następująco:
+
+W części zmiennych mamy takie pola jak:
+- listę zawierającą wszystkie taski,
+- zawartość startową pola tekstowego.
+
+W części metod mamy takie metody jak:
+- metodę dodającą task,
+- metodę usuwającą task,
+- metodę zmieniającą stan taska na skończony, badź odwrotnie.
+
+![obraz](https://user-images.githubusercontent.com/56678518/150826279-825d21a8-771b-4fc9-8102-61924f0adcf0.png)
+
+
+### Zmiany w projekcie
+
+Wygląd strony startowej po zmianach:
+
+Zmiany wprowadzone w projekcie:
+- Dodanie komunikatu jeśli nie mamy żadnych tasków (za pomocą `v-if`/`v-if-else`)
+- Dodanie zliczania ilości wszystkich tasków,
+- Dodanie zliczania ilości tasków gotowych,
+- wyliczanie procenta ukończonych tasków
+
+![obraz](https://user-images.githubusercontent.com/56678518/150827199-32139f4b-4f32-41db-994a-0e4c2d841ba7.png)
+
+#### Dodanie komunikatu jeśli nie mamy żadnych tasków (za pomocą `v-if`/`v-if-else`)
+
+Zmieniony kod sprawdza czy ilość tasków (taskOverall) jest większy od zera lub czy wszystkie taski są ukończone.
+
+Warunek v-else-if sprawdza czy wszystkie taski są ukończone.
+Te dwa warunki decydują czy zostanie wyświetlony komunika o ukończonych taskach, czy komunikat wyświetalający statystyki
+odnoście naszych tasków.
+
+![obraz](https://user-images.githubusercontent.com/56678518/150827604-c1e09a3c-cf10-428e-bf81-23a06459e5d1.png)
+
+Widok aplikacji kiedy są dostępne jakieś taski do zrobienia:
+
+![obraz](https://user-images.githubusercontent.com/56678518/150828095-68a95d70-071a-4e98-872d-9e931cc92064.png)
+
+Wartości obliczanie są za pomocą nowo napisanej metody `calcPercent` wywoływanej przy każdej aktualizacji ToDo listy.
+
+![obraz](https://user-images.githubusercontent.com/56678518/150828345-df963a9b-d4eb-4990-a6f6-8d40c5aaf876.png)
+
+Próbowałem wykorzystać gotowe już komponenty do wyświetlania progressu w postaci wypełniającego się okręgu, niestety wyszytkie gotowe
+materiały były tak źle zrobione, że procent zapełnienia koła był jedynie ustawiany na load'zie strony, a zmiany wartości
+w czasie późniejszym w kodzie nie dawały efektów.
+
+Wypróbowane metody wdrożenia progress-baru:
+- https://www.npmjs.com/package/vue2-circle-progress
+- https://www.npmjs.com/package/vue-radial-progress
+- https://bestofvue.com/repo/delowardev-vue3-circle-progress-vuejs-progress-bar
+
+Wykorzystane moduły:
+- bootstrap
+- bootstrap-vue
+
+Wykorzystane komponenty:
+Wykorzystany został jedynie napisany przez autora komponent SFC o nazwie App, który zawiera wszystkie funkcjonalności strony
+
+### Git difftool
+
+Wykorzystanie polecenia Git difftool wyświetla nam następujące rezultaty: <br>
+Możemy dowiedzieć się, że do div'a o klasie row justify-content-center dodane 2 nowe divy z właściowościami v-if i v-else-if. Możemy po tym pliku scrollować do końca pliku.
+
+![obraz](https://user-images.githubusercontent.com/56678518/150833604-845aa18b-79a1-485c-89ad-91a60a77f6e9.png)
+
+Niestety dla mnie narzędzie to nie jest zbyt przejrzyste. Sam wykorzystuję narzędzie Github desktop, które pozwala mi na wyświetlanie
+tej komendy w czasie rzeczywistym, oraz na commitowanie, pushowanie, fetchowanie itp.
+
+Aby w pełni zobrazować zmiany wykonane na projekcie posłużę się tym narzędziem.
+
+![obraz](https://user-images.githubusercontent.com/56678518/150834251-1c07a4b8-8772-4006-a113-5e89e6592ca6.png)
+
+W tej przejrzystej, nowoczesnej formie możemy zobaczyć, że dodane zostały 2 nowe pliki (oznaczone na zielono) i 4 zostały zmodyfikowane
+(oznaczone na żółto). Na prawym panelu widzimy również wszystkie zmiany w kodzie, gdzie na czerowno oznaczony jest kod który został zmieniony/wycięty z kodu, a na zielono kod dodany.
+
+
+
+
+
+
+
+
+
--- src/App.vue
+++ src/App.vue
@@ -1,83 +1,130 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png" width="75px" height="75px">
+    <img src="./assets/logo.png" width="75px" height="75px" />
     <h1 class="mt-3">Vue.js Todo List</h1>
     <div class="container">
-
       <section>
-         <div class="row justify-content-center mt-4">
-           <input v-model="inputField" v-on:keyup.enter="addTodo" class="mr-1" placeholder="Todo Item" />
-           <button @click="addTodo" class="btn btn-primary">Add Todo</button>
+        <div class="row justify-content-center mt-4">
+          <input
+            v-model="inputField"
+            v-on:keyup.enter="addTodo"
+            class="mr-1"
+            placeholder="Todo Item"
+          />
+          <button @click="addTodo" class="btn btn-primary">Add Todo</button>
+        </div>
+        <div v-if ="tasksOverall > 0 && percent != 100" class="percShow">
+          <p>Tasks done: {{ tasksDone }}
+          Total Tasks: {{ tasksOverall }} <br> Task percentage: {{ percent }}%</p>
+        </div>
+        <div v-else-if = "percent == 100" class="percShow">
+         <p> All finished, job done :) </p>
         </div>
       </section>
-
-       <section class="container">
-          <div class="row">
-             <div class="offset-md-3 col-md-6 mt-3">
-                <ul class="list-group justify-content-center">
-                   <li class="row list-group-item border mt-2 col-xs-1" v-for="todo in todoList">
-                      <div class="row align-items-center">
-                        <input type="checkbox" v-on:change="toggle(todo)" v-bind:checked="todo.complete" class="col-sm-1 border border-danger">
-                        <del v-if="todo.complete" class="col-sm-8">
-                           <h5>{{ todo.name }}</h5>
-                        </del>
-                        <span v-else class="col-sm-8">
-                           <h5>{{ todo.name }}</h5>
-                        </span>
-                        <span @click="deleteTodo(todo)" class="offset-sm-1 col-sm-2 delete text-right">X</span>
-                      </div>
-                   </li>
-                </ul>
-             </div>
+      <section class="container">
+        <div class="row">
+          <div class="offset-md-3 col-md-6 mt-3">
+            <ul class="list-group justify-content-center">
+              <li
+                class="row list-group-item border mt-2 col-xs-1"
+                v-for="todo in todoList"
+              >
+                <div class="row align-items-center">
+                  <input
+                    type="checkbox"
+                    v-on:change="toggle(todo)"
+                    v-bind:checked="todo.complete"
+                    class="col-sm-1 border border-danger"
+                  />
+                  <del v-if="todo.complete" class="col-sm-8">
+                    <h5>{{ todo.name }}</h5>
+                  </del>
+                  <span v-else class="col-sm-8">
+                    <h5>{{ todo.name }}</h5>
+                  </span>
+                  <span
+                    @click="deleteTodo(todo)"
+                    class="offset-sm-1 col-sm-2 delete text-right"
+                    >X</span
+                  >
+                </div>
+              </li>
+            </ul>
           </div>
-       </section>
-
+        </div>
+      </section>
     </div>
-
   </div>
 </template>
 
 <script>
-import Vue from 'vue';
-import BootstrapVue from 'bootstrap-vue';
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Vue from "vue";
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import VueCircle from "vue2-circle-progress";
 
-import test from './components/test';
+import "./assets/main.css";
+
+import test from "./components/test";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-     test
- },
+    test,
+  },
+  components: { VueCircle },
+  data() {
+    return {
+      percent: 100,
+      tasksDone: 0,
+      tasksOverall: 0,
+      inputField: "",
+      todoList: [],
+      fill: { gradient: ["red", "green", "blue"] },
+    };
+  },
   methods: {
-    addTodo: function(todo) {
+    addTodo: function (todo) {
       todo = this.inputField;
-      this.todoList.push({name: todo, complete: false});
-      this.inputField = '';
-      console.log(this.todoList);
-   },
-   deleteTodo: function(todo) {
+      this.todoList.push({ name: todo, complete: false });
+      this.inputField = "";
+      this.calcPercent();
+    },
+    deleteTodo: function (todo) {
       var index = this.todoList.indexOf(todo);
       this.todoList.splice(index, 1);
-      console.log(this.todoList);
-   },
-   toggle: function(todo) {
+      this.calcPercent();
+    },
+    toggle: function (todo) {
       todo.complete = !todo.complete;
-   }
- },
-  data () {
-    return {
-      inputField: '',
-      todoList: []
-    }
-  }
-}
+      this.calcPercent();
+    },
+    calcPercent: function () {
+      var perc = 0;
+      var num = this.todoList.length;
+      this.todoList.forEach((element) => {
+        if (element.complete) {
+          perc++;
+        }
+      });
+      this.tasksDone = perc;
+      this.tasksOverall = num;
+      this.percent = Math.floor((perc / num) * 100);
+    },
+    progress(event, progress, stepValue) {
+      console.log(stepValue);
+    },
+    progress_end(event) {
+      console.log("Circle progress end");
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -85,12 +132,13 @@ export default {
   margin-top: 60px;
 }
 
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 
 h5 {
-   margin-bottom: 0px;
+  margin-bottom: 0px;
 }
 
 ul {
@@ -108,11 +156,11 @@ a {
 }
 
 .delete {
-   color: pink;
-   cursor: pointer;
+  color: pink;
+  cursor: pointer;
 }
 
 .delete:hover {
-   color: red;
+  color: red;
 }
 </style>
